493f95b65873b3cd7512305ad3fc45e73925c969
Stanislav Borozna <ssborozna@avito.ru>
2025-03-08 23:30:49 +0300

    Removed unnecessary api methods

--- frontend/microfrontend/profile/src/utils/api.js
+++ frontend/microfrontend/profile/src/utils/api.js
@@ -36,16 +36,6 @@ class Api {
             .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
     }
 
-    removeCard(cardID) {
-        return fetch(`${this._address}/${this._groupId}/cards/${cardID}`, {
-            method: 'DELETE',
-            headers: {
-                authorization: this._token,
-            },
-        })
-            .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
-    }
-
     setUserInfo({ name, about }) {
         return fetch(`${this._address}/${this._groupId}/users/me`, {
             method: 'PATCH',
@@ -71,18 +61,6 @@ class Api {
             body: JSON.stringify({
                 avatar,
             }),
-        })
-            .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
-    }
-
-    changeLikeCardStatus(cardID, like) {
-        // Обычная реализация: 2 разных метода для удаления и постановки лайка.
-        return fetch(`${this._address}/${this._groupId}/cards/like/${cardID}`, {
-            method: like ? 'PUT' : 'DELETE',
-            headers: {
-                authorization: this._token,
-                'Content-Type': 'application/json',
-            },
         })
             .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
     }
